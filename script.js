49bb5856e8baedfd14642b1c029c3af31dac6e75
rischaelin <ri_schaelin@bluewin.ch>
2017-02-11 10:40:12 +0100

    techf

--- script.js
+++ script.js
@@ -5,3 +5,27 @@ var ageDifference = "7";
 
 
 console.log(age + ageDifference);
+
+var a = 6;
+var b = 5;
+var c = a + b;
+
+console.log(c)
+
+
+
+var run = function run(callback) {
+    console.log("run() wurde ausgeführt");
+    callback();
+
+}
+
+var f = function () {
+    console.log("f() wurde ausgeführt");
+}
+run(f);
+
+setTimeout(function () {
+    console.log("funtion() wurde ausgeführt");
+}, 2000)
+
