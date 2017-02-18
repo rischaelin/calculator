b599522adb3c16dc28bdc566e2b396a0e21940ae
rischaelin <ri_schaelin@bluewin.ch>
2017-02-18 20:40:15 +0100

    addition

--- script.js
+++ script.js
@@ -1,31 +1,66 @@
 "use strict";
 
-var age = 25;
-var ageDifference = "7";
+
+$().ready(function () {
+
+    //meine Elemente
+    var $inputA = $("#A");
+    var $inputB = $("#B");
+    var $btPlus = $("#btPlus");
+    var $resultGo = $("#resultGo");
+    var $btMinus = $("#btMinus");
+
+    /*Ergebnis
+     inputA plus inputB
+     */
 
 
-console.log(age + ageDifference);
+    //bei click auf den buttonGo
+    $btPlus.on('click', function () {
+        //umgewandelte zahlen addieren
+        var ergebnis = addieren($inputA.val(), $inputB.val());
+        // ausgeben in
+        console.log(ergebnis);
+        alert(ergebnis);
+        schreibeSumme(ergebnis);
+        /*var $bt = $(this);
+        $bt.hide();
+        setTimeout(function () {
+            $bt.show();
+        },5000);*/
+    });
 
-var a = 6;
-var b = 5;
-var c = a + b;
 
-console.log(c)
+    $btMinus.on ('click', function () {
+        var ausgabe = subtrahieren($inputA.val(), $inputB.val());
+        //ausgeben in
+        console.log(ausgabe);
+        alert(ausgabe);
+        schreibeMinus(ausgabe);
+
+    });
+
+    //function für resultGo
+    function schreibeSumme(ergebnis) {
+        $resultGo.html(ergebnis);
+    };
+
+    function schreibeMinus(ausgabe) {
+        $resultGo.html(ausgabe);
+    };
 
 
 
-var run = function run(callback) {
-    console.log("run() wurde ausgeführt");
-    callback();
 
-}
 
-var f = function () {
-    console.log("f() wurde ausgeführt");
-}
-run(f);
 
-setTimeout(function () {
-    console.log("funtion() wurde ausgeführt");
-}, 2000)
+});
 
+//inhalt von a + b text in Zahl umwandeln
+var addieren = function (a, b) {
+    return parseInt(a) + parseInt(b)
+};
+
+var subtrahieren = function (a, b) {
+    return parseInt(a) - parseInt(b)
+};
